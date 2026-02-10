88e25ea15c805b4d1d602e9ca23cf9df67d8fb4a
Syed Mehdi <syed@bcw.group>
2026-02-10 09:41:28 +0500

    URL patch: add arkhia API key url

--- counter-cli/src/config.ts
+++ counter-cli/src/config.ts
@@ -52,12 +52,13 @@ export class PreviewConfig implements Config {
   }
 }
 
+export const PROOF_SERVER_URL_POSTFIX = '/midnight/zkpaas/testnet/46634Y77zrsb1294Z72h9P02MN43d4N4';
 export class PreprodConfig implements Config {
   logDir = path.resolve(currentDir, '..', 'logs', 'preprod', `${new Date().toISOString()}.log`);
   indexer = 'https://indexer.preprod.midnight.network/api/v3/graphql';
   indexerWS = 'wss://indexer.preprod.midnight.network/api/v3/graphql/ws';
   node = 'https://rpc.preprod.midnight.network';
-  proofServer = 'http://127.0.0.1:6300';
+  proofServer = 'https://starter.qa.arkhia.network' + PROOF_SERVER_URL_POSTFIX;
   constructor() {
     setNetworkId('preprod');
   }
